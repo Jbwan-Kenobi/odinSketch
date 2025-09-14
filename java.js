43c0db02d9245cb09ecb1af85da9ef1dabc386ca
Jbwan-Kenobi <171935619+Jbwan-Kenobi@users.noreply.github.com>
2025-09-14 20:53:45 +0100

    Apply height and width to divs

--- java.js
+++ java.js
@@ -1,6 +1,10 @@
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 32; i++) {
     var div =  document.createElement("div");
     div.classList.add("blocks");
     div.textContent = "div";
+    div.style.color = "blue";
+    div.style.width = "250px";
+    div.style.height = "250px";
+    div.style.justifyContent = "centre";
     container.appendChild(div);
 };
